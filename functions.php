52b184bfd4197ed3393146478d4df64db38fa29f
Syed Zaman Mostafiz <zamanwebdev@gmail.com>
2022-12-09 23:14:06 +0600

    Wordpress Theme Development part 03 running

--- functions.php
+++ functions.php
@@ -5,4 +5,12 @@
 // Theme Title
 add_theme_support('title-tag');
 
-?>
+// Theme CSS and jQuery File Calling
+function zaman_css_js_file_caling(){
+    wp_enqueue_style('zaman-style', get_stylesheet_uri());
+    // wp_register_style( 'bootstrap', get_template_directory_uri().'/css/bootstrap.css', array(), '5.0.2', 'all' );
+    // wp_enqueue_style('bootstrap');
+    // wp_register_style( 'custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.0', 'all' );
+    // wp_enqueue_style('custom');
+}
+add_action( 'wp_enqueue_scripts', 'zaman_css_js_file_caling');
