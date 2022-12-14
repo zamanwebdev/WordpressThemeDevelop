3d4323254770db649d056f7469437876c70e3ad2
Syed Zaman Mostafiz <zamanwebdev@gmail.com>
2022-12-14 23:11:06 +0600

    Part 08 Completed.

--- index.php
+++ index.php
@@ -13,20 +13,27 @@
    <?php wp_head( )?>
 </head>
 <body <?php body_class();?>>
-    <div id="header_area" class="<?php echo get_theme_mod('zaman_menu_position'); ?>">
+    <header id="header_area" class="<?php echo get_theme_mod('zaman_menu_position'); ?>">
         <div class="container">
             <div class="row">
                 <div class="col-md-3">
-                    <a href=""><img src="<?php echo get_theme_mod('zaman_logo'); ?>" alt=""></a>
+                    <a href="<?php echo home_url(); ?>"><img src="<?php echo get_theme_mod('zaman_logo'); ?>" alt=""></a>
                 </div>
                 <div class="col-md-9">
                     <?php wp_nav_menu( array('theme_location' => 'main_menu', 'menu_id' => 'nav') ); ?>
                 </div>
             </div>
         </div>
+    </header>
+    <section id="body_area">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <?php the_content(); ?>
+        </div>
+      </div>
     </div>
-
-
+  </section>
 
     <?php wp_footer( )?>
 </body>
