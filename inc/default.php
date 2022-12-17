f86b2ca41afc6d2f01db5ef47fc9755cbc125f82
Syed Zaman Mostafiz <zamanwebdev@gmail.com>
2022-12-17 16:48:06 +0600

    the_excerpt

--- inc/default.php
+++ inc/default.php
@@ -6,3 +6,15 @@ add_theme_support('title-tag');
 // Thumbnil Image Area
 add_theme_support( 'post-thumbnails', array('page', 'post') );
 add_image_size('post-thumbnails', 970, 350, true);
+
+// Except to 40 Word
+
+function zaman_excerpt_more($more){
+  return '<br> <br> <a class="redmore" href="'.get_permalink( $post->ID) . '">' . 'Read More' . '</a>';
+}
+add_filter('excerpt_more', 'zaman_excerpt_more');
+
+function zaman_excerpt_lenght($length){
+  return 40;
+}
+add_filter('excerpt_length', 'zaman_excerpt_lenght', 999);
